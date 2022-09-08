5ed666289266af3165f889973e5da4c7781b7b66
Ken Mwangi <kenmwangi071@gmail.com>
2022-09-08 00:58:40 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,11 @@
  * main function - prints "Programming is like building a multilingual puzzle"
  * Returns 0; if exited properly - non-zero incase of error
  */
+
+/* main function */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+	/* returns 0 */
 	return (0);
 }
