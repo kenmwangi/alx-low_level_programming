a0bf680021b5254da36bf6c1b5d903191e983685
Ken Mwangi <kenmwangi071@gmail.com>
2022-09-09 09:11:43 -0700

    4. Task alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,10 +1,10 @@
-nclude <stdio.h>
+#include <stdio.h>
 
 /**
  *  * main - prints the alphabet in lowercase,
  *   * followed by a new line, except q and e
  *    * Return: Aways 0 (Sucess)
- *     */
+ */
 int main(void)
 {
 	char ch = 'a';
