76d54be5d13d93dc28b3206f7083e94c9b6899d7
Ken Mwangi <kenmwangi071@gmail.com>
2022-09-15 12:13:15 -0700

    12. Print Numbers12. Print Numbers12. Print Numbers12. Print Numbers12. Print Numbers12. Print Numbers12. Print Numbers12. Print Numbers12. Print Numbers12. Print Numbers12. Print Numbers12. Print Numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -13,12 +13,16 @@ void print_number(int n)
 		n1 = -n;
 		_putchar('-');
 	} else
-	{
-		if (n1 / 10)
-		{
-			print_number(n1 / 10);
-		}
-
-		_putchar((n1 % 10) + '0');
+	{ 
+		n1 = n;
 	}
+
+	
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+
+	_putchar((n1 % 10) + '0');
+	
 }
