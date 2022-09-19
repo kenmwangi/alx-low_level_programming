9c56c9871ec7d3e87d34a990f402882fe13a9ca9
Ken Mwangi <kenmwangi071@gmail.com>
2022-09-19 04:33:58 -0700

    2. _strlen- get string length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,7 +2,9 @@
 
 /**
  *  * _strlen - returns the length of a string
- *  * Return: the length of the string
+ *   * @s: string to evaluate
+ *    *
+ *     * Return: the length of the string
  */
 int _strlen(char *s)
 {
