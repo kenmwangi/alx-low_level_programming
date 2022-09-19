2b945780923af960d371443eaf0c4e59ef2dc628
Ken Mwangi <kenmwangi071@gmail.com>
2022-09-19 05:30:02 -0700

    6. print string characters

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
-/*
- * puts2 - prints one char out of 2 of a string
- * \n new line
- * @str: string to display chars
-*/
+/**
+ *  puts2 - prints one char out of 2 of a string
+ *  followed by a new line
+ *  @str: string to print the chars from
+ */
 void puts2(char *str)
 {
 	int i;
@@ -19,7 +19,7 @@ void puts2(char *str)
 	{
 		_putchar(str[i]);
 	}
-	_putchar('\n');
 
-	
+
+	_putchar('\n');
 }
