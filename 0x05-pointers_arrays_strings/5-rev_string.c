9856f55492a2bd59bec4d57c89a4fa1845b8653b
Ken Mwangi <kenmwangi071@gmail.com>
2022-09-19 05:12:57 -0700

    5. reversing a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,10 +6,24 @@
  */
 void rev_string(char *s)
 {
+	char tmp;
+	int i;
+	int len = 0;
+	int len1 = 0;
 
-	char s[23] = "Ken";
+	while (s[len] != '\0')
+	{
+		len++;
+	}
 
-	strrev(s);
+	len1 = len - 1;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len1];
+		s[len1--] = tmp;
+	}
 
 
 
