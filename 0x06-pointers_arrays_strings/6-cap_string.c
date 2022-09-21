472faf56bcd00242b9a1dab5c1e4d69c446ce4eb
Ken Mwangi <kenmwangi071@gmail.com>
2022-09-21 09:43:43 -0700

    6. Capitalize all words in string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,28 +7,29 @@
  */
 char *cap_string(char *s)
 {
-	int i;
+	int i = 0;
 	int j;
 
-	char spe[13] = {'', '\t', '\n',',',';','.','!','?','"','(', ')','{', '}'};
+	char a[] = " \t\n,;.!?\"(){}";
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (*(s + i))
 	{
-		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
 		{
-			s[i] -= 32;
-		}
-
-		for (j = 0; j < 13; j++)
-		{
-			if (s[i] == spe[j])
+			if (i == 0)
+				*(s + i) -= 'a' - 'A';
+			else
 			{
-				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				for (j = 0; j <= 12; j++)
 				{
-					s[i + 1] -= 32;
+					if (a[j] == *(s + i - 1))
+					{
+						*(s + i) -= 'a' - 'A';
+					}
 				}
 			}
 		}
+		i++;
 	}
 
 	return (s);
