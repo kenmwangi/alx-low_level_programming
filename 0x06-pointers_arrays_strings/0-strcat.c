8b5499ac3aa40ea6cd490c896b11f1aa4f16ad33
Ken Mwangi <kenmwangi071@gmail.com>
2022-09-21 09:22:08 -0700

    0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,18 +11,13 @@ char *_strcat(char *dest, char *src)
 	int i = 0;
 	int j = 0;
 
-	while (dest[i] != '\0')
+	while (*(dest + i))
 		i++;
-
-	while (src[j] != '\0')
+	while ((*(dest + i) = *(src + j)))
 	{
-		dest[i] = src[j];
-		j++;
 		i++;
+		j++;
 	}
-
-	dest[i] '\0';
-
 	return (dest);
 
 }
