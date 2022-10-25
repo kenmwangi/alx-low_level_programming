1085db345a54233ef70960e4fb7e8c3281ccf636
Ken Mwangi <kenmwangi071@gmail.com>
2022-10-25 00:15:53 -0700

    prototypes functions

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -8,6 +8,9 @@
  *  @next: points to the next node
  *  Description: singly linked list node structure
  */
+
+#include <stdlib.h>
+
 typedef struct list_s
 {
 	char *str;
@@ -19,6 +22,6 @@ size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *d);
+void free_list(list_t *head);
 
 #endif
