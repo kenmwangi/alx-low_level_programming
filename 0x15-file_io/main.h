48ba7b1d1720c1a54c9d1c4d1dcf69baf07e6061
Ken Mwangi <kenmwangi071@gmail.com>
2022-11-02 10:23:25 -0700

    prototype functions

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,17 +1,12 @@
-#ifndef STDLIB_H
-#define STDLIB_H
-#include <stdlib.h>
-#endif
+#ifndef FILE_IO
+#define FILE_IO
 
-#ifndef UNISTD_H
-#define UNISTD_H
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
-#endif
 
-
-#ifndef _MAIN_H_
-#define _MAIN_H_
-
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
